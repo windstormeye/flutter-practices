7e626444bf4c2073e2159d56d6988ce56dc32ec3
windstormeye <877302410@qq.com>
2019-01-10 18:20:27 +0800

    [flutter] <init> name generator app

---
+++ test/app/src/main/java/com/pjhubs/test/HomeActivity.kt
@@ -0,0 +1,22 @@
+package com.pjhubs.test
+
+import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity;
+
+import kotlinx.android.synthetic.main.activity_home.*
+
+class HomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+        setSupportActionBar(toolbar)
+
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+    }
+
+}
